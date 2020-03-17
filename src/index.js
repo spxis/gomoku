f7c8e949c61ad40ec438914900ccf9edee09f12a
John Morris <john@spxis.com>
2020-03-17 12:50:09 -0700

    Updated logic. Added Skip turn button. Do both horizontal and vertical checking. Minor issues (edge cases) found in checker to be fixed later.

--- src/index.js
+++ src/index.js
@@ -15,10 +15,20 @@ function Square(props) {
 function ResetGameButton(props) {
     return (
         <button
-            title="Reset"
+            title="Reset Game"
             className="button"
             onClick={props.resetBoard}
         >Reset</button>
+    );
+}
+
+function SkipTurnButton(props) {
+    return (
+        <button
+            title="Skip Turn"
+            className="button"
+            onClick={props.skipTurn}
+        >Skip Turn</button>
     );
 }
 
@@ -28,6 +38,7 @@ class Board extends React.Component {
         this.rows = props.rows;
         this.columns = props.columns;
         this.board = props.board;
+        this.players = props.players;
         this.playerIsBlack = props.playerIsBlack;
     }
 
@@ -64,7 +75,7 @@ class Board extends React.Component {
         const rowCount = this.rows;
         const colCount = this.columns;
 
-        let status = 'Next player: ' + (this.props.playerIsBlack ? 'X' : 'O');
+        let status = 'Next up: Player ' + (this.props.playerIsBlack ? this.players.one : this.players.two);
 
         const colIds = Array.from(Array(colCount), (d, i) => i);
         let rows = [];
@@ -85,18 +96,26 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
 
-        this.board_rows = 12;
-        this.board_columns = 12;
+        this.board_rows = 19;
+        this.board_columns = 19;
+        this.cellsRequiredToWin = 5;
+        this.exactMatchRequired = true;
 
         this.createBoard = this.createBoard.bind(this);
         this.resetBoard = this.resetBoard.bind(this);
+        this.skipTurn = this.skipTurn.bind(this);
         this.clickCell = this.clickCell.bind(this);
 
         this.board = this.createBoard(this.board_rows, this.board_columns);
         this.playerIsBlack = true;
+        this.players = {
+            one: 'X',
+            two: 'O',
+        };
 
         this.state = {
             board: this.board,
+            players: this.players,
             playerIsBlack: this.playerIsBlack
         };
     }
@@ -115,20 +134,81 @@ class Game extends React.Component {
     resetBoard() {
         this.board = this.createBoard(this.board_columns, this.board_rows);
         this.playerIsBlack = true;
-        // this.forceUpdate();
         this.setState({
             board: this.board,
             playerIsBlack: this.playerIsBlack,
         });
     }
 
+    skipTurn() {
+        this.playerIsBlack = !this.state.playerIsBlack;
+        this.setState({
+            playerIsBlack: this.playerIsBlack,
+        });
+    }
+
     clickCell(x, y) {
         const board = [...this.board];
-        board[x][y] = this.state.playerIsBlack ? 'X' : 'O';
+        board[x][y] = this.state.playerIsBlack ? this.players.one : this.players.two;
         this.setState({
             board: board,
             playerIsBlack: !this.state.playerIsBlack
         });
+    }
+
+    checkForWin(board, player, rules, direction = 'horizontal') {
+        let isWinner = false;
+        let counter = 0;
+
+        for (let x = 0; x < board.length; x++) {
+            for (let y = 0; y < board[x].length; y++) {
+                const cell = direction === 'horizontal' ? board[x][y] : board[y][x];
+                if (rules.exactMatchRequired) {
+                    if (cell === player && counter <= rules.cellsRequiredToWin) {
+                        counter++;
+                    } else if (counter > rules.cellsRequiredToWin) {
+                        isWinner = false;
+                        break;
+                    } else if (counter === rules.cellsRequiredToWin) {
+                        isWinner = true;
+                        break;
+                    } else {
+                        isWinner = false;
+                        counter = 0;
+                    }
+                } else {
+                    if (cell === player && counter < rules.cellsRequiredToWin) {
+                        counter++;
+                    } else if (counter === rules.cellsRequiredToWin) {
+                        isWinner = true;
+                        break;
+                    } else {
+                        isWinner = false;
+                        counter = 0;
+                    }
+                }
+                if (isWinner) {
+                    break;
+                }
+            }
+            if (isWinner) {
+                break;
+            }
+        }
+
+        return isWinner;
+    }
+
+    hasWin(player) {
+        const rules = {
+            exactMatchRequired: this.exactMatchRequired,
+            cellsRequiredToWin: this.cellsRequiredToWin
+        };
+
+        let isXWinner = this.checkForWin(this.board, player, rules, 'horizontal');
+        let isYWinner = this.checkForWin(this.board, player, rules, 'vertical');
+
+        return isXWinner || isYWinner;
     }
 
     render() {
@@ -139,19 +219,32 @@ class Game extends React.Component {
                         rows={this.board_rows}
                         columns={this.board_columns}
                         board={this.state.board}
+                        players={this.state.players}
                         playerIsBlack={this.state.playerIsBlack}
                         clickCell={this.clickCell}
                     />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    Game Status:
+                    <ul>
+                        <li>Player {this.players.one}: {
+                            this.hasWin(this.players.one) ? 'WIN' : '-'
+                        }</li>
+                        <li>Player {this.players.two}: {
+                            this.hasWin(this.players.two) ? 'WIN' : '-'
+                        }</li>
+                    </ul>
                 </div>
                 <div className="game-panel">
                     <ResetGameButton
                         rows={this.board_rows}
                         columns={this.board_columns}
                         resetBoard={this.resetBoard}
+                    />
+                    <SkipTurnButton
+                        board={this.state.board}
+                        playerIsBlack={this.state.playerIsBlack}
+                        skipTurn={this.skipTurn}
                     />
                 </div>
             </div>
